ea74db9c6629cc9eb9101b5ac397f8055a2b9047
FooKaDoO <mihkelhani@gmail.com>
2022-11-01 19:34:09 +0200

    fixed

--- res/js/posts.js
+++ res/js/posts.js
@@ -6,13 +6,20 @@ fetch('http://myjson.dit.upm.es/api/bins/5gy2')
 for (let i = 0; i < json.length; i++) {
 
     let rida = json[i];
-    posts.innerHTML += '<li><div class="profile">'+
-    '<img src="res/img/'+ rida.name + rida.lastname +'.jpeg" alt="pfp">'+
-    '<p>'+ rida.date + '</p></div><div class="comment">';
-    if (rida.attachment)
-        posts.innerHTML += '<img src="res/img/'+ rida.postID +'.jpeg" alt="My picture">';
-    posts.innerHTML += '<p>'+ rida.text + '</p></div></li>';
-     
+    if (rida.attachment) {
+        posts.innerHTML += '<li><div class="profile">'+
+        '<img src="res/img/'+ rida.name + rida.lastname +'.jpeg" alt="pfp">'+
+        '<p>'+ rida.date + '</p></div><div class="comment">'+
+        '<img src="res/img/'+ rida.postID +'.jpeg" alt="My picture">'+
+        '<p>'+ rida.text + '</p></div></li>';
+    }
+    else { // For some reason this does work and "if" sentence only on adding image doesn't.
+        posts.innerHTML += '<li><div class="profile">'+
+        '<img src="res/img/'+ rida.name + rida.lastname +'.jpeg" alt="pfp">'+
+        '<p>'+ rida.date + '</p></div><div class="comment">'+
+        '<p>'+ rida.text + '</p></div></li>';
+    }
+    
     }
     }
 )
