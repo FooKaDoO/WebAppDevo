a2f570dc1061673181a99ec608a99ceb01feb114
ottericottender <ott.eric.ottender@ut.ee>
2022-11-01 22:25:28 +0200

    Dropdown menu addition

--- res/js/posts.js
+++ res/js/posts.js
@@ -1,3 +1,22 @@
+/**This function is for the dropdown menu*/
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
 const posts = document.getElementById("joemama");
 
 fetch('http://myjson.dit.upm.es/api/bins/gm6i')
