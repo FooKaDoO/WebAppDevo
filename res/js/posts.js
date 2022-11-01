d140af0a3cd8d9a3c57072b66aedac1a27d85428
FooKaDoO <mihkelhani@gmail.com>
2022-11-01 19:27:49 +0200

    if attachment

--- res/js/posts.js
+++ res/js/posts.js
@@ -1,6 +1,6 @@
 const posts = document.getElementById("joemama");
 
-fetch('http://myjson.dit.upm.es/api/bins/h0ua')
+fetch('http://myjson.dit.upm.es/api/bins/5gy2')
 .then((response) => response.json())
 .then(json => {
 for (let i = 0; i < json.length; i++) {
@@ -8,9 +8,10 @@ for (let i = 0; i < json.length; i++) {
     let rida = json[i];
     posts.innerHTML += '<li><div class="profile">'+
     '<img src="res/img/'+ rida.name + rida.lastname +'.jpeg" alt="pfp">'+
-    '<p>'+ rida.date + '</p></div><div class="comment">'+
-    '<img src="res/img/'+ rida.postID +'.jpeg" alt="My picture">'+
-    '<p>'+ rida.text + '</p></div></li>';
+    '<p>'+ rida.date + '</p></div><div class="comment">';
+    if (rida.attachment)
+        posts.innerHTML += '<img src="res/img/'+ rida.postID +'.jpeg" alt="My picture">';
+    posts.innerHTML += '<p>'+ rida.text + '</p></div></li>';
      
     }
     }
