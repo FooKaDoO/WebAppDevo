dc7c9627ea7021f47530a8e12fc106299645c7f2
FooKaDoO <mihkelhani@gmail.com>
2022-11-01 19:21:14 +0200

    fetch posts

--- res/js/posts.js
+++ res/js/posts.js
@@ -1,8 +1,17 @@
-// meie json: http://myjson.dit.upm.es/api/bins/h0ua
-
 const posts = document.getElementById("joemama");
-posts.innerHTML += '<li><div class="profile">'+
-    '<img src="res/img/'+ 'sun_baby' +/*pfp*/'.jpeg" alt="pfp">'+
-    '<p>'+ 'date' + /*kuupäev*/'</p></div><div class="comment">'+
-    '<img src="res/img/'+ 'sun_baby' +/*attachment*/'.jpeg" alt="My picture">'+
-    '<p>'+ 'Lolololsmolaolslpo' +/*post text*/'</p></div></li>';
+
+fetch('http://myjson.dit.upm.es/api/bins/h0ua')
+.then((response) => response.json())
+.then(json => {
+for (let i = 0; i < json.length; i++) {
+
+    let rida = json[i];
+    posts.innerHTML += '<li><div class="profile">'+
+    '<img src="res/img/'+ rida.name + rida.lastname +'.jpeg" alt="pfp">'+
+    '<p>'+ rida.date + '</p></div><div class="comment">'+
+    '<img src="res/img/'+ rida.postID +'.jpeg" alt="My picture">'+
+    '<p>'+ rida.text + '</p></div></li>';
+     
+    }
+    }
+)
