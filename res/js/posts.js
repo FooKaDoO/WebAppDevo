8d0582f9087d14099e4feb4eeca7a601466ac138
FooKaDoO <mihkelhani@gmail.com>
2022-11-01 19:10:55 +0200

    postID=JOEMAMAAAA

---
+++ res/js/posts.js
@@ -0,0 +1,8 @@
+// meie json: http://myjson.dit.upm.es/api/bins/h0ua
+
+const posts = document.getElementById("joemama");
+posts.innerHTML += "<li><h1>fuck u</h1></li>";'<li><div class="profile">'+
+    '<img src="res/img/'+ 'sun_baby' +/*pfp*/'.jpeg" alt="pfp">'+
+    '<p>'+ 'date' + /*kuupäev*/'</p></div><div class="comment">'+
+    '<img src="res/img/'+ 'sun_baby' +/*attachment*/'.jpeg" alt="My picture">'+
+    '<p>'+ 'Lolololsmolaolslpo' +/*post text*/'</p></div></li>';
